eccb60bc93e50a787e646b54ad6a66bdfea0578b
magdaU <magdalena.ukleja@gmail.com>
2019-03-04 18:27:17 +0100

    Frontend with JSP

--- spring-mvc-demo/src/com/github/magdau/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/com/github/magdau/springdemo/mvc/Student.java
@@ -7,7 +7,17 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private String country;
+	private String favoriteLanguage;
+	private String [] operatingSystems;
 	
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
+	}
+
 	private LinkedHashMap<String, String> countryOptions;
 	
 	public String getCountry() {
@@ -49,5 +59,13 @@ public class Student {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
 	}
 }

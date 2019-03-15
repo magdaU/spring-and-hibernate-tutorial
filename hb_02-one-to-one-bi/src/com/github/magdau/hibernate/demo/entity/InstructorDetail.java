f4e9c2f6abd0409204e54a23a85dea45eab48d75
magdaU <magdalena.ukleja@gmail.com>
2019-03-15 12:16:42 +0100

    @OneToOne Hibernate - BI-Directional -Delete Only Instructr Details

--- hb_02-one-to-one-bi/src/com/github/magdau/hibernate/demo/entity/InstructorDetail.java
+++ hb_02-one-to-one-bi/src/com/github/magdau/hibernate/demo/entity/InstructorDetail.java
@@ -40,7 +40,8 @@ public class InstructorDetail {
 	
 	// add @OneToOne annotation
 	
-	@OneToOne(mappedBy="instructorDetail", cascade=CascadeType.ALL)
+	@OneToOne(mappedBy="instructorDetail",
+			cascade= {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
 	private Instructor instructor;
 
 	

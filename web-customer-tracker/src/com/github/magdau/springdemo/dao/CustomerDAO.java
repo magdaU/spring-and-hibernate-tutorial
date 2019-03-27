c3a3cbe452d6c5312e4799fa07e7ce4b9d729d3e
magdaU <magdalena.ukleja@gmail.com>
2019-03-27 12:07:48 +0100

    Create Hibernate DAO

---
+++ web-customer-tracker/src/com/github/magdau/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,14 @@
+package com.github.magdau.springdemo.dao;
+
+import java.util.List;
+
+import com.github.magdau.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+	
+	
+	
+
+}

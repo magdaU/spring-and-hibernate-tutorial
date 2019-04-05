67725221429f008d590ee861ecb6de6c848bcc22
magdaU <magdalena.ukleja@gmail.com>
2019-04-05 13:52:58 +0200

    AOP - Pointcut Expressions Match any Return Type

--- spring-demo-aop/src/com/github/magdau/aopdemo/dao/MembershipDAO.java
+++ spring-demo-aop/src/com/github/magdau/aopdemo/dao/MembershipDAO.java
@@ -5,9 +5,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class MembershipDAO {
 	
-	public void addSillyMember () {
+	public boolean addSillyMember () {
 		
 		System.out.println(getClass() + " : Doing stuff: adding a memebership account ");
+		return true;
 	}
 
 }

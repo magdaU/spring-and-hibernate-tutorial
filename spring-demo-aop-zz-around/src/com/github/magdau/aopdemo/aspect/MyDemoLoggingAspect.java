16b167037662c9c5471928bdcd7b481387ae013d
magdaU <magdalena.ukleja@gmail.com>
2019-04-14 17:01:38 +0200

    Aop : @Around part 2

--- spring-demo-aop-zz-around/src/com/github/magdau/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-zz-around/src/com/github/magdau/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,9 +3,11 @@ package com.github.magdau.aopdemo.aspect;
 import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -18,8 +20,32 @@ import com.github.magdau.aopdemo.Account;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+	
+	@Around("execution(* com.github.magdau.aopdemo.service.*.getFortune(..))")
+	public Object aroundGetFortune(
+			ProceedingJoinPoint theProceedingJoinPoint) throws Throwable{
+				
+		//print out method we are adivising on
+		String method= theProceedingJoinPoint.getSignature().toShortString();
+		System.out.println("\n ====> Executing @Around  on method " + method);
 		
-	//After will run for success or faillure(finally)
+		//get begin timestamp
+		long begin = System.currentTimeMillis();
+		
+		//now, let's execute the method
+		Object result= theProceedingJoinPoint.proceed();
+		
+		//get end timestamp
+		long end = System.currentTimeMillis();
+		
+		//compute duration and display it
+		long duration = end - begin;
+		System.out.println("\n====> Duration: " + duration / 1000.00 + " seconds");
+		
+		return result;
+	}
+		
+	//After will run for success or failure(finally)
 	@After("execution(* com.github.magdau.aopdemo.dao.AccountDAO.findAccounts(..))")
 		public void afterFinallyFindAccountAdvice(JoinPoint theJointPoint) {
 		

8ae7ba3ccaa7dd210fe8880f24620298f48bd27b
magdaU <magdalena.ukleja@gmail.com>
2019-05-08 13:12:13 +0200

    Access Denied

---
+++ spring-security-demo-07-user-roles/spring-security-demo-07-user-roles/src/main/java/com/github/magdau/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.github.magdau.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}

6abda05b661138abfbac04b6b415c61b3a46dd7a
magdaU <magdalena.ukleja@gmail.com>
2019-05-09 14:19:46 +0200

    Spring Security JDBC - Coding Reading Props Files

--- spring-security-demo-08-jdbc-plaintext/src/main/java/com/github/magdau/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-demo-08-jdbc-plaintext/src/main/java/com/github/magdau/springsecurity/demo/config/DemoAppConfig.java
@@ -1,8 +1,15 @@
 package com.github.magdau.springsecurity.demo.config;
 
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -10,7 +17,15 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.github.magdau.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties") // src/main/resources - files are automatically copied to classpath during Maven build
 public class DemoAppConfig {
+	
+	//set up variable to hold the properties
+	@Autowired
+	private Environment env; // will hold data read from properties files
+	
+	//set up a logger for diagnostics
+	private Logger logger= Logger.getLogger(getClass().getName());
 	
 	// define a bean for a ViewRoselwer
 	
@@ -25,4 +40,22 @@ public class DemoAppConfig {
 		return viewResolver;
 	}
 
+	//define a bean for our security datasource
+	
+	@Bean
+	public DataSource securityDataSource() {
+		
+		//create connection pool
+		
+		//set the jdb driver class
+		
+		//log the connection props
+		
+		//set database connection props
+		
+		//set connection pool props
+	
+		
+		return null;
+	}
 }

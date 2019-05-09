8cabf75daa0e888c5cb0d1a9e76967be067e5602
magdaU <magdalena.ukleja@gmail.com>
2019-05-09 14:47:07 +0200

    Spring Security JDBC - Add JDBC Authentication

--- spring-security-demo-08-jdbc-plaintext/src/main/java/com/github/magdau/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-demo-08-jdbc-plaintext/src/main/java/com/github/magdau/springsecurity/demo/config/DemoAppConfig.java
@@ -19,54 +19,78 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages="com.github.magdau.springsecurity.demo")
-@PropertySource("classpath:persistence-mysql.properties") // src/main/resources - files are automatically copied to classpath during Maven build
+@ComponentScan(basePackages = "com.github.magdau.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties") // src/main/resources - files are automatically copied to
+															// classpath during Maven build
 public class DemoAppConfig {
-	
-	//set up variable to hold the properties
+
+	// set up variable to hold the properties
 	@Autowired
 	private Environment env; // will hold data read from properties files
-	
-	//set up a logger for diagnostics
-	private Logger logger= Logger.getLogger(getClass().getName());
-	
+
+	// set up a logger for diagnostics
+	private Logger logger = Logger.getLogger(getClass().getName());
+
 	// define a bean for a ViewRoselwer
-	
+
 	@Bean
 	public ViewResolver viewResolver() {
-		
+
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		
+
 		viewResolver.setPrefix("/WEB-INF/view/");
 		viewResolver.setSuffix(".jsp");
-		
+
 		return viewResolver;
 	}
 
-	//define a bean for our security datasource
-	
+	// define a bean for our security datasource
+
 	@Bean
 	public DataSource securityDataSource() {
-		
-		//create connection pool
+
+		// create connection pool
 		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
-		
-		//set the jdb driver class
+
+		// set the jdb driver class
 		try {
 			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
 		} catch (PropertyVetoException exc) {
 			throw new RuntimeException(exc);
 		}
-		
-		//log the connection props
-		//for sanity's sake, log this info
-		//just to make sure we are Really reading data from properties file
+
+		// log the connection props
+		// for sanity's sake, log this info
+		// just to make sure we are Really reading data from properties file
 		logger.info(">>>jdbc.url=" + env.getProperty("jdbc.url"));
 		logger.info(">>>jdbc.user=" + env.getProperty("jdbc.user"));
-		//set database connection props
-		
-		//set connection pool props
-			
+
+		// set database connection props
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setUser(env.getProperty("jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+		// set connection pool props
+		securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+
+		securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.minPoolSize"));
+
+		securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+
+		securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.maxIdleTime"));
+
 		return securityDataSource;
+	}
+
+	// nedd a helper method
+	// read environemnt property and convert to int
+
+	private int getIntProperty(String propName) {
+		String propVal = env.getProperty(propName);
+
+		// now convert to int
+		int intPropVal = Integer.parseInt(propVal);
+
+		return intPropVal;
 	}
 }
--- spring-security-demo-08-jdbc-plaintext/src/main/java/com/github/magdau/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-08-jdbc-plaintext/src/main/java/com/github/magdau/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.github.magdau.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,16 +14,18 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	//add a references to our security data source
+	@Autowired
+	private DataSource securityDataSource;
+	
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		// add our users for in memory authentication
-		UserBuilder users = User.withDefaultPasswordEncoder();
-
-		auth.inMemoryAuthentication().withUser(users.username("john").password("test123").roles("Employee"))
-				.withUser(users.username("mary").password("test123").roles("Employee", "Manager"))
-				.withUser(users.username("susan").password("test123").roles("Employee", "Admin"));
+		
+		//use jdbc authentication ... oh yeah !
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+		
 	}
 
 	@Override

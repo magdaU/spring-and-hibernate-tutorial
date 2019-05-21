eabc5e274d506eae115c3866f6b7b66e58836e16
magdaU <magdalena.ukleja@gmail.com>
2019-05-21 11:13:02 +0200

    02-dev-tools-demo

---
+++ 02-dev-tools-demo/src/main/java/com/github/magdau/springboot/mycoowebapp/MycoowebappApplication.java
@@ -0,0 +1,13 @@
+package com.github.magdau.springboot.mycoowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoowebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoowebappApplication.class, args);
+	}
+
+}

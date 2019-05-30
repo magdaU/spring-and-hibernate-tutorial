bc54156202ad65f130558a954ebea8fe292016fc
magdaU <magdalena.ukleja@gmail.com>
2019-05-30 13:21:56 +0200

    Sping Boot - Thymeleaf - Crud Database Project - Update Employee part 1

---
+++ 36-thymeleafdemo-employees-update/src/main/java/com/github/springboot/thymleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.github.springboot.thymleafdemo.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.github.springboot.thymleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	//that's it... no need to write any code LOL!
+	
+	//add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
+}

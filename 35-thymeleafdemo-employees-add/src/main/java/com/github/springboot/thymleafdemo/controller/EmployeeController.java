de5150148f2e3359270ce6902bf5a6a336a7f641
magdaU <magdalena.ukleja@gmail.com>
2019-05-30 12:50:04 +0200

    Spring Boot - Thymeleaf - Add Employee part 3

--- 35-thymeleafdemo-employees-add/src/main/java/com/github/springboot/thymleafdemo/controller/EmployeeController.java
+++ 35-thymeleafdemo-employees-add/src/main/java/com/github/springboot/thymleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.github.springboot.thymleafdemo.entity.Employee;
@@ -15,23 +17,44 @@ import com.github.springboot.thymleafdemo.service.EmployeeService;
 public class EmployeeController {
 
 	private EmployeeService employeeService;
-	
-	public EmployeeController(EmployeeService theEmployeeService) {
-		employeeService=theEmployeeService;
-	}
 
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
+	}
 
 	// add mapping for "/list"
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
 
-		//get employees from db
-		List<Employee> theEmployees= employeeService.findAll();
-		
+		// get employees from db
+		List<Employee> theEmployees = employeeService.findAll();
+
 		// add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 
 		return "employees/list-employees";
 	}
 
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+
+		// create model attribute to bind form data
+		Employee theEmployee = new Employee();
+
+		theModel.addAttribute("employee", theEmployee);
+
+		return "employees/employee-form";
+
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		
+		// save the employee
+		employeeService.save(theEmployee);
+		
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
+	}
+	
 }
--- 35-thymeleafdemo-employees-add/src/main/java/com/github/springboot/thymleafdemo/dao/EmployeeRepository.java
+++ 35-thymeleafdemo-employees-add/src/main/java/com/github/springboot/thymleafdemo/dao/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.github.springboot.thymleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.springboot.thymleafdemo.entity.Employee;
@@ -7,4 +9,7 @@ import com.github.springboot.thymleafdemo.entity.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	//that's it... no need to write any code LOL!
+	
+	//add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
 }

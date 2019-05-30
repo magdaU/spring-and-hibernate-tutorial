19e686b9353c8c46a412fc05158d4e5a1876587b
magdaU <magdalena.ukleja@gmail.com>
2019-05-30 13:37:35 +0200

    Spring Boot - Thymeleaf - CRUD Database - Update Employee part 2

--- 36-thymeleafdemo-employees-update/src/main/java/com/github/springboot/thymleafdemo/controller/EmployeeController.java
+++ 36-thymeleafdemo-employees-update/src/main/java/com/github/springboot/thymleafdemo/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.github.springboot.thymleafdemo.entity.Employee;
 import com.github.springboot.thymleafdemo.service.EmployeeService;
@@ -47,14 +48,27 @@ public class EmployeeController {
 
 	}
 
+	@GetMapping("showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+
+		// get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
+
+		// set employee as a model attribute to pre-populate the form
+		theModel.addAttribute("employee", theEmployee);
+
+		// send over to our form
+		return "employees/employee-form";
+	}
+
 	@PostMapping("/save")
 	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
-		
+
 		// save the employee
 		employeeService.save(theEmployee);
-		
+
 		// use a redirect to prevent duplicate submissions
 		return "redirect:/employees/list";
 	}
-	
+
 }
